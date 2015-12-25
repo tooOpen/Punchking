d177d9a5bd49e2ea1aa46516a1b106d02f3c411e
soonhang Byun <sabjil0608@naver.com>
2015-12-25 15:33:46 +0900

    delete old one

--- PunchKing/proj.android/gen/com/punchking/game/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.punchking.game;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- PunchKing/proj.android/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/ContactListener.o.d
+++
@@ -1,2 +0,0 @@
-obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/ContactListener.o: \
- jni/../../Classes/ContactListener.cpp
